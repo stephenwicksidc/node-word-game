0fe64cda49878e43c9ca2fa5c425ce840b7f9179
Stephen Wicks <stevewicks1@gmail.com>
2017-06-25 19:39:44 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const fs = require('fs');
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+let spacesArray = ["", "", "", "", "", "", ""];
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(express.static('public'));
+
+app.use(session({
+  secret: 'tROi$ e+ D3uX Et 1',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set ('view engine', 'mustache');
+
+app.get('/', function(req, res) {
+  res.render("index");
+});
+
+app.listen(3000, function() {
+  console.log("Working hard... Listening on 3000");
+});
